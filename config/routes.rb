d219b745e2f5526d8a799f123b8761f37431282a
matsu8232 <252098197+matsu8232@users.noreply.github.com>
2026-03-01 17:53:23 +0900

    [update]ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'items/new'
-  post 'items' => 'items#create'
-  devise_for :users
+  resources :items
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
