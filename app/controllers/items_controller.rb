e5c89e911b6bfc5cbf7a15b1427b912f1717ce52
matsu8232 <252098197+matsu8232@users.noreply.github.com>
2026-03-01 18:41:27 +0900

    [update]削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,6 +27,12 @@ class ItemsController < ApplicationController
     redirect_to item_path(item.id)
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to items_path
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :status)
