6e4ccb4a855efe5b52f5394e5b4d48c33000db44
matsu8232 <252098197+matsu8232@users.noreply.github.com>
2026-03-02 10:33:33 +0900

    [update]新規登録・ログインリンク

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :info])
   end
+
+  def after_sign_in_path_for(resource)
+    items_path
+  end
   
 end
