078ee75c9ba82bdcea1915b8c0cee0e74a177e13
matsu8232 <252098197+matsu8232@users.noreply.github.com>
2026-03-08 19:27:11 +0900

    [update]バリデーション追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,10 +4,13 @@ class ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new(item_params)
-    item.user_id = current_user.id
-    item.save
-    redirect_to item_path(item.id)
+    @item = Item.new(item_params)
+    @item.user_id = current_user.id
+    if @item.save
+      redirect_to item_path(@item.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
@@ -23,9 +26,12 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to item_path(item.id)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,7 @@ class Item < ApplicationRecord
   enum status: { available: 0, maintenance: 1 }
   has_one_attached :image
   has_many :reservations, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :description, presence: true, length: { maximum: 200}
 end
