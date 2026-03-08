9a08750b1318f64c71c05f94a5dc30173c3704b4
matsu8232 <252098197+matsu8232@users.noreply.github.com>
2026-03-08 18:16:10 +0900

    [update]ユーザ編集ページの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @items = @user.items.page(params[:page]).per(8).reverse_order
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :info, :avatar)
+  end
 end
