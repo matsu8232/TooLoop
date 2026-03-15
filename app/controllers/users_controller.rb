01dccbd22ff719b8cb56aec11781f2f589b2fbd2
matsu8232 <252098197+matsu8232@users.noreply.github.com>
2026-03-15 23:42:31 +0900

    [update]予約履歴表示機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @items = @user.items.page(params[:page]).per(8).reverse_order
+
+    @future_reservations = @user.reservations.where('end_date >= ?', Date.current).includes(:item).order(start_date: :asc)
+    @past_reservations = @user.reservations.where('end_date < ?', Date.current).includes(:item).order(start_date: :desc)
   end
 
   def edit
