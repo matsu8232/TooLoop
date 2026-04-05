cfb8c8c38723a18b23f2e8b3cf65d4855ab85179
matsu8232 <252098197+matsu8232@users.noreply.github.com>
2026-04-05 16:18:31 +0900

    [fix]コード誤り修正

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
   has_many :entries, dependent: :destroy
-  has_many :chat, dependent: :destroy
+  has_many :chats, dependent: :destroy
   has_many :users, through: :entries
 end
